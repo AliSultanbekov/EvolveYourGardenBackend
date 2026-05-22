f37760172a2a107c0a53c898d12a7badd471561a
Curse <thehackgamemap@gmail.com>
2026-05-22 01:25:30 +0100

    update

--- src/main/java/com/evolveyourgarden/weather_service/WeatherController.java
+++ src/main/java/com/evolveyourgarden/weather_service/WeatherController.java
@@ -1,4 +1,18 @@
 package com.evolveyourgarden.weather_service;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/weather")
+    public WeatherState GetWeather() {
+        return weatherService.GetCurrentWeather();
+    }
 }
