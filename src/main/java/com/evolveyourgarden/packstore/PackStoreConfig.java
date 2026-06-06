ce6c57b7230bd1496d66c7ffac3898296c512435
Curse <thehackgamemap@gmail.com>
2026-06-06 03:44:34 +0100

    update

--- src/main/java/com/evolveyourgarden/packstore/PackStoreConfig.java
+++ src/main/java/com/evolveyourgarden/packstore/PackStoreConfig.java
@@ -10,10 +10,6 @@ public final class PackStoreConfig {
             "Special", new Category(3, Map.of(
                     "Super Pack", new PackDef(50, 1, 2),
                     "Mega Pack",  new PackDef(50, 1, 2)
-            )),
-            "Event", new Category(3, Map.of(
-                    "Elite Pack",     new PackDef(30, 1, 1),
-                    "Legendary Pack", new PackDef(10, 1, 1)
             ))
     );
 
